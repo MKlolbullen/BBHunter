3a48dbd081a23cbdebe35e360d9c7efbfcd474ce
MKlolbullen <53312929+MKlolbullen@users.noreply.github.com>
2024-11-11 13:22:18 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,28 @@
 # config.py
 
+import os
+
 class Config:
     SECRET_KEY = 'your_secret_key_here'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    OUTPUT_DIR = '/path/to/output'
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    OUTPUT_DIR = os.path.join(BASE_DIR, 'scan_results')
+
+    # Paths to the external tools
     TOOL_PATHS = {
         'httpx': '/usr/local/bin/httpx',
         'katana': '/usr/local/bin/katana',
         'gospider': '/usr/local/bin/gospider',
         'gau': '/usr/local/bin/gau',
-        'chaos': '/usr/local/bin/chaos',
         'assetfinder': '/usr/local/bin/assetfinder',
-        'subfinder': '/usr/local/bin/subfinder'
+        'subfinder': '/usr/local/bin/subfinder',
+        'dnsx': '/usr/local/bin/dnsx',
+        'amass': '/usr/bin/amass',
+        'nuclei': '/usr/local/bin/nuclei',
     }
+
+    # Celery configuration
+    CELERY_BROKER_URL = 'redis://localhost:6379/0'  # Adjust if needed
+    CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
