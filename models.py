a1f3427a5ccdd40c7a22a43c098f79d8cb25fd12
MKlolbullen <53312929+MKlolbullen@users.noreply.github.com>
2024-11-11 13:29:14 +0100

    Update models.py

--- models.py
+++ models.py
@@ -2,13 +2,31 @@
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from config import Config
 
 db = SQLAlchemy()
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'user'  # Specify table name for clarity
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), nullable=False, unique=True)
-    password = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String(200), nullable=False)  # Store hashed passwords
+    registered_on = db.Column(db.DateTime, server_default=db.func.now())
+
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(Config.SECRET_KEY, expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(Config.SECRET_KEY)
+        try:
+            user_id = s.loads(token)['user_id']
+        except Exception:
+            return None
+        return User.query.get(user_id)
 
 class ScanResult(db.Model):
     id = db.Column(db.Integer, primary_key=True)
