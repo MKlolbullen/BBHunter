7ef259d8ba34833f852833da8a6376bce189ec47
MKlolbullen <53312929+MKlolbullen@users.noreply.github.com>
2025-02-16 02:42:25 +0100

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,31 +1,16 @@
-# forms.py
-
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from models import User
-from flask_login import current_user
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=150)])
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=150)])
     email = StringField('Email', validators=[DataRequired(), Email(), Length(max=150)])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=150)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
-    # Custom validators to check if username or email already exists
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user:
-            raise ValidationError('Username already exists. Please choose another.')
-
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user:
-            raise ValidationError('Email already registered. Please use a different email.')
-
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=150)])
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=150)])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
@@ -33,9 +18,3 @@ class LoginForm(FlaskForm):
 class UpdateProfileForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email(), Length(max=150)])
     submit = SubmitField('Update')
-
-    def validate_email(self, email):
-        if email.data != current_user.email:
-            user = User.query.filter_by(email=email.data).first()
-            if user:
-                raise ValidationError('Email already registered. Please use a different email.')
